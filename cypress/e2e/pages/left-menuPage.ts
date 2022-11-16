9294e6bbb1ecb3336c64598c74d27320f9878462
SergT <stbn135@gmail.com>
2022-11-17 00:53:04 +0100

    add return

---
+++ cypress/e2e/pages/left-menuPage.ts
@@ -0,0 +1,10 @@
+export class LeftMenuPage{
+
+    private optionHome = 'a[data-test="sidenav-home"]';
+    //a[data-test="sidenav-home"] span:contains('Home')
+    private optionUserMyAcc= 'a[data-test="sidenav-user-settings"] ';
+    private optionBankAcc = 'a[data-test="sidenav-home"]';
+    private optionNotifc = 'a[data-test="sidenav-notifications"]';
+    private optionLogout = 'a[data-test="sidenav-signout"]';    
+
+}
--- cypress/e2e/pages/loginPage.ts
+++ cypress/e2e/pages/loginPage.ts
@@ -1,50 +1,57 @@
 /// <reference types = "cypress"/>
 
 import { Button } from "./button";
+import {LeftMenuPage} from "./left-menuPage"
 
 export class LoginPage {
     private userNameField: string = 'input[name="username"]';
     private passwordField: string = 'input[name="password"]';
     private rememberMeCheckBox: string  = 'input[name="remember"]';
-    public signInButton: string = 'button[type="submit"]';
+    private signInButton: string = 'button[type="submit"]';
     private signUpReff: string = 'a[data-test="signup"]';
     public signedUserFullName: string = '[data-test="sidenav-user-full-name"]'; // full name of signed Up user
-    public userInvalid: string = '[data-test="signin-error"]'; // invalid user aler
+    public userInvalid: string = '[data-test="signin-error"]'; // invalid user alert
 
         
-    private signInButton_:Button = new Button();
-    private  cssButton =  this.signInButton_.css = 'button[type="submit"]';
+    private signInButton_: Button = new Button();
+    private  cssButton = this.signInButton_.css = 'button[type="submit"]';
     
-    public newclick(){
-        this.signInButton_.click();       
+    public newclick(): LoginPage{
+        this.signInButton_.click(); 
+        return this;      
     }
    
 
-    public enterUserName(username: string) {
+    public enterUserName(username: string): LoginPage {
         cy.get(this.userNameField).type(username);
+        return this;
     }
 
-    public enterpassword(pass: string) {
+    public enterpassword(pass: string): LoginPage {
         cy.get(this.passwordField).type(pass);
+        return this;
     }
 
-    public clickSignIn() {
+    public clickSignIn(): LoginPage {
         cy.get(this.signInButton).click();
+        return this;
     }
 
-    public login(username: string, pass: string) {
-        this.enterUserName(username);
-        this.enterpassword(pass);
-        //this.clickSignIn();
-        this.newclick();
+    public login(username: string, pass: string): LoginPage {
+        this.enterUserName(username)
+        .enterpassword(pass)
+        .newclick();
+        //.clickSignIn();       
+        return this;
     }
 
-    public loginAuthUser() {
+    public loginAuthUser(): LeftMenuPage {
         cy.fixture('database.json').then((data) => {
             //var q_users = data['users'].length; 
             let randomUserNum: number = Math.floor(Math.random() * (data['users'].length));
             let randomUserName: string = data.users[randomUserNum].username;
             this.login(randomUserName, "s3cret");
         })
+        return new LeftMenuPage();
     }
 }
